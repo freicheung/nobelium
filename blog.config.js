052f1e8808a6d3e4296815eb79a0e6c9d7e162ab
freicheung <77823688+freicheung@users.noreply.github.com>
2021-05-05 21:10:30 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,8 +1,8 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
-  link: 'https://nobelium.vercel.app',
+  title: 'one.two.three',
+  author: 'Kevin Cheung',
+  email: 'immerda@gmail.com',
+  link: 'https://freiheit.vercel.app',
   description: 'This gonna be an awesome website.',
   lang: 'en-US',
   appearance: 'auto', // ['light', 'dark', 'auto'],
